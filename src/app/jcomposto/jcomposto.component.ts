d406548a467039c92b3da57fc030937905afbba9
marjciardullo <marjoryeramirez@gmail.com>
2022-03-18 20:39:41 -0300

    2 files modified

--- src/app/jcomposto/jcomposto.component.ts
+++ src/app/jcomposto/jcomposto.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-jcomposto',
   templateUrl: './jcomposto.component.html',
-  styleUrls: ['./jcomposto.component.css']
+  styleUrls: ['./jcomposto.component.css'],
 })
 export class JcompostoComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  va: number;
+  tj: number;
+  n: number;
+  constructor() {
+    this.va = 25000;
+    this.tj = 3;
+    this.n = 6;
   }
 
+  ngOnInit() {}
 }
